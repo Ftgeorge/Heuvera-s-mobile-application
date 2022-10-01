ca8bbaffe33ce1d871d7ede2ae2fef2b51d37bdb
Ftgeorge <ftgeorgean@gmail.com>
2022-10-01 15:12:37 +0100

    Day 4

---
+++ lib/TabPages/places.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class places extends StatefulWidget {
+  const places({super.key});
+
+  @override
+  State<places> createState() => _placesState();
+}
+
+class _placesState extends State<places> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
